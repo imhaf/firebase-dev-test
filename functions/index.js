61a4288dcf003d391c6e34d3810adcc081cae774
Mukhsin Fahmi <mukhsin.fahmi@gmail.com>
2022-09-30 10:36:03 +0700

    Convert ISBN to image_url function

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,12 @@
 // Create cloud function to convert the ISBN number to a link and save it in the database
+const functions = require('firebase-functions');
+
+exports.convertISBN = functions.firestore.document('books/{bookId}').onCreate((snap, context) => {
+    const isbn = snap.data().isbn;
+
+    if (isbn) {
+        const url = `https://covers.openlibrary.org/b/isbn/${ isbn }-S.jpg`;
+        return snap.ref.set({image_url: url}, {merge: true});
+    }
+    return 'Not adding image URL!';
+});
