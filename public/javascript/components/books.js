86cd386141b28acf89193215d0d0901bf4ad2598
Mukhsin Fahmi <mukhsin.fahmi@gmail.com>
2022-09-30 10:37:05 +0700

    Update image_url

--- public/javascript/components/books.js
+++ public/javascript/components/books.js
@@ -1,7 +1,7 @@
 import { html, render } from 'uhtml';
 import menu from '../extensions/menu';
 import { getApp } from 'firebase/app';
-import { getFirestore, collection, query, getDocsFromServer } from 'firebase/firestore';
+import { getFirestore, collection, query, getDocsFromServer, updateDoc, doc, getDoc } from 'firebase/firestore';
 
 export default {
   path: '/books',
@@ -9,14 +9,23 @@ export default {
   callback: async(update) => {
 
     const database = getFirestore(getApp());
+    const updateBook = async () => {
+      const docRef = doc(database, "books/OH8glJGrD6a6aFVqZj01");
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.data().isbn) {
+        await updateDoc(docRef, {
+          isbn: "0140092501"
+        });
+      }
+    }
+    await updateBook();
     const books = await getDocsFromServer(query(collection(database, 'books')));
-
     let content = html`<p>There are no books available</p>`;
 
     function thumbnail(item) {
-      const { isbn } = item.data();
+      const { isbn, image_url } = item.data();
       if (isbn) {
-        const url = `https://covers.openlibrary.org/b/isbn/${ item.data().isbn }-S.jpg`;
+        const url = image_url || `https://covers.openlibrary.org/b/isbn/${ item.data().isbn }-S.jpg`;
         return html`<img src="${ url }" />`;
       }
       return '';
